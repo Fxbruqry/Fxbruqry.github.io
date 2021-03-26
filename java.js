7f3225ba8ef013d59a5506d615f044a6bb0f4334
Fxbruqry <e-Mayaga@iskule.no>
2021-03-26 14:05:48 +0100

    melding

--- java.js
+++ java.js
@@ -6,5 +6,5 @@ function changeColor(){
 function changeBackgroundColor(){
     var body = document.getElementsByTagName("body")[0];
     var colorSelect = document.getElementById("bcolor");
-    body.style.backgroundcolor = colorSelect.options[colorSelect.selectedIndex].value; 
+    body.style.backgroundColor = colorSelect.options[colorSelect.selectedIndex].value; 
 }
