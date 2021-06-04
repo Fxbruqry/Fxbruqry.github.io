0d982f672bb45635913e71f23810717dec6bccb0
Fxbruqry <e-Mayaga@iskule.no>
2021-06-04 11:09:31 +0200

    melding

---
+++ resources/js/ayoo.js
@@ -0,0 +1,8 @@
+var biler = ["Volvo", "Saab", "BMW", "Toyota", "Mitsubishi", "Peugeot", "Honda", "Nissan"];
+document.getElementById("bil").innerHTML = fruits;
+
+function myFunction() {
+  biler.sort();
+  document.getElementById("bil").innerHTML = fruits;
+}
+</script>
